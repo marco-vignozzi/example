d24ca27b10c3044ecc5bc6112021152650bbd509
marco-vignozzi <marcovignozzi7@gmail.com>
2022-02-19 14:48:59 +0100

    added regiopregio

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "mainwindow.h"
-
+#include <iostream>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    std::cout << "RegioPregio" << std::endl;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
